eeac0614f28e389131c0c35a01278378bc3c18a5
James Horton <38235491+jayaimzzz@users.noreply.github.com>
2018-12-19 15:44:08 -0500

    Revert "dummy data is in state in the redux store"

--- src/Reducers/AnimalsReducer.js
+++
@@ -1,9 +0,0 @@
-import { animals } from '../dummyData'
-
-const initState = animals
-
-export const AnimalsReducer = (state = initState, action) => {
-    switch(action.type){
-        default: return state
-    }
-}
--- src/Reducers/HostsReducer.js
+++
@@ -1,9 +0,0 @@
-import { hosts } from '../dummyData';
-
-const initState = hosts;
-
-export const HostsReducer = (state = initState, action) => {
-    switch(action.type){
-        default: return state
-    }
-}
