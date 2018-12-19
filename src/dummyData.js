a32eb568e7bd9c9d6bb59bf052803b42d2bf5742
jayaimzzz <jameshorton@gmail.com>
2018-12-19 17:04:19 -0500

    changed phone numbers to strings. added shelterId prop to animal

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,8 +1,9 @@
 export let animals = [
   {
-    id: 1,
+    id: "1",
     name: "Toby",
     dob: "2015-03-21",
+    shelterId: "1",
     hostId: "1",
     status: "adoptable",
     species: 'dog',
@@ -25,10 +26,10 @@ export let animals = [
 export let hosts = [
   
   {
-      id: 2,
+      id: "1",
       name: 'Kathy',
       address: "459 Roundabout way",
-      phoneNumber: 5555551234,
+      phoneNumber: "5555551234",
       photos: ["https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiZjPLErazfAhUq7IMKHXUdBAwQjRx6BAgBEAU&url=http%3A%2F%2Fwww.arottalove.net%2Fblog%2F2008%2F08%2Fcrazy-dog-lady%2F&psig=AOvVaw2zQhYuLmZBFpHsh7mGnArI&ust=1545324769681218"],
       capacity: {cats: 0, dogs: 2}
   }
@@ -36,10 +37,10 @@ export let hosts = [
 
 export let shelters = [
     {
-        id: 1,
+        id: "1",
         name: 'HAND',
         address: "123 Main Street",
-        phoneNumber: 8881234321,
+        phoneNumber: "8881234321",
         photos: ["https://i.pinimg.com/originals/97/19/9c/97199cdda2fec20471eb88c8da150220.jpg"],
         capacity: {cats: 100, dogs: 50}
       }
