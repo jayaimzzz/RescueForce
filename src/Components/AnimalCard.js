4a09c719581458a3ef39f1a509cee253457a5385
James Horton <38235491+jayaimzzz@users.noreply.github.com>
2018-12-19 15:52:05 -0500

    Revert "Animal card"

--- src/Components/AnimalCard.js
+++ src/Components/AnimalCard.js
@@ -1,20 +1,7 @@
 import React from "react";
-import "../styles/ImageDisplayer.css";
-import stockIcon from "../styles/photos/stockIcon.gif";
-import { Card } from "semantic-ui-react";
 
-class AnimalCard extends React.Component {
-  render() {
-    return (
-      <Card color="green" className="animalCard">
-        <ImageDisplayer />
-        <Card.Header>Doggie's Name</Card.Header>
-        <Card.Meta>DOB</Card.Meta>
-        <Card.Description>Breed, Age, ???</Card.Description>
-        <Card.Footer>Host</Card.Footer>
-      </Card>
-    );
-  }
-}
-
-export default AnimalCard;
+export const AnimalCard = ({ animal }) => (
+  <div style={{ height: 80, backgroundColor: "blue", margin: 10 }}>
+    {animal}
+  </div>
+);
