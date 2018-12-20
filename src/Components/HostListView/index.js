7c53a8a4772ee03e423c9fcc95d6989cfade920d
Carl Smith <carlfsmithiii@gmail.com>
2018-12-20 13:09:06 -0500

    add HostCard components to HostListView

--- src/Components/HostListView/index.js
+++ src/Components/HostListView/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import { HostCard } from "./HostCard";
+
+const tempDevelopmentHosts = ["one", "two", "three", "four"];
 
 class HostListView extends Component {
   render() {
     return (
       <div style={{ height: 200, backgroundColor: "orange", margin: 10 }}>
         Host List View
+        {tempDevelopmentHosts.map(host => (
+          <HostCard key={host} host={host} />
+        ))}
       </div>
     );
   }
