819a34ef1618706504680028eb39b1bbc76e922a
jayaimzzz <jameshorton@gmail.com>
2018-12-20 18:11:24 -0500

    adjusted image size

--- src/Components/AnimalProfileView/ImageScroll.js
+++ src/Components/AnimalProfileView/ImageScroll.js
@@ -1,35 +1,42 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-const imgStyles = {
-  display: "inline-block",
-  borderRadius: "5px"
-}
-
-const segmentStyles = {
-  overflowX: "scroll",
-  whiteSpace: "nowrap",
-  borderRadius: "5px"
-}
+const styles = {
+  img: {
+    display: "inline-block",
+    borderRadius: "5px",
+    transition: "transform .2s"
+  },
+  segment: {
+    overflowX: "scroll",
+    whiteSpace: "nowrap",
+    borderRadius: "5px"
+  },
+};
 
 class ImageScroll extends Component {
   render() {
-    console.log(this.props)
-    const images = this.props.images
-    return(
-      <div class="ui segment" style={segmentStyles}>
-        {images.map(image => <img style={imgStyles} class="ui small image bordered" src={image}></img>)}
+    console.log(this.props);
+    const images = this.props.images;
+    return (
+      <div class="ui segment" style={styles.segment}>
+        {images.map(image => (
+          <img style={styles.img} class="ui medium image bordered" src={image}/>
+        ))}
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state, props) => {
   return {
     images: state.animals.find(animal => animal.id == props.animalId).pictures
-  }
-}
+  };
+};
 
 const mapDispatchToProps = null;
 
-export default connect(mapStateToProps, mapDispatchToProps)(ImageScroll)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ImageScroll);
