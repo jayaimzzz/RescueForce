bd06082676561b11c08cdcc1c425b562da136ed8
jayaimzzz <jameshorton@gmail.com>
2018-12-20 17:25:22 -0500

    basic image scrolling is working

--- src/Components/AnimalProfileView/ImageScroll.js
+++ src/Components/AnimalProfileView/ImageScroll.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export const ImageScroll = () => (
-  <div style={{ height: 80, backgroundColor: "darkcyan" }}>
-    Images of The Animal
-  </div>
-);
+const imgStyles = {
+  display: "inline-block",
+  borderRadius: "5px"
+}
+
+const segmentStyles = {
+  overflowX: "scroll",
+  whiteSpace: "nowrap",
+  borderRadius: "5px"
+}
+
+class ImageScroll extends Component {
+  render() {
+    console.log(this.props)
+    const images = this.props.images
+    return(
+      <div class="ui segment" style={segmentStyles}>
+        {images.map(image => <img style={imgStyles} class="ui small image bordered" src={image}></img>)}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  return {
+    images: state.animals.find(animal => animal.id == props.animalId).pictures
+  }
+}
+
+const mapDispatchToProps = null;
+
+export default connect(mapStateToProps, mapDispatchToProps)(ImageScroll)
