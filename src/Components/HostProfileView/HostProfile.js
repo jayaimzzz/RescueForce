a845cdaa7757d4b029e5258646226edc42b77409
Carl Smith <carlfsmithiii@gmail.com>
2018-12-20 12:42:38 -0500

    move AnimalList and AnimalCard to AnimalList directory, add host profile and animal list to host profile view

---
+++ src/Components/HostProfileView/HostProfile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const HostProfile = () => (
+  <div
+    style={{
+      display: "inline-block",
+      backgroundColor: "BurlyWood",
+      height: 200,
+      width: "40vw"
+    }}
+  >
+    Host Profile
+  </div>
+);
--- src/Components/HostProfileView/index.js
+++ src/Components/HostProfileView/index.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
+import { HostProfile } from "./HostProfile";
+import AnimalList from "../AnimalListView/AnimalList";
 
 class HostProfileView extends Component {
-    render() {
-        return (
-            <div style={{ height: 200, backgroundColor: "red", margin: 10 }}>Host Profile View</div>
-        );
-    };
+  render() {
+    return (
+      <Fragment>
+        <div style={{ height: 200, backgroundColor: "red", margin: 10 }}>
+          Host Profile View
+        </div>
+        <div style={{ float: "left", width: "fit-content" }}>
+          <HostProfile />
+        </div>
+        <div style={{ float: "left" }}>
+          <AnimalList host="hostId" />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export { HostProfileView };
