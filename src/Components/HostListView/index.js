92756f3dcb0e18cf635e566a31955d2760a69044
Carl Smith <carlfsmithiii@gmail.com>
2018-12-20 13:11:35 -0500

    add HostFilter to HostListView

--- src/Components/HostListView/index.js
+++ src/Components/HostListView/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { HostCard } from "./HostCard";
+import { HostFilter } from "./HostFilter";
 
 const tempDevelopmentHosts = ["one", "two", "three", "four"];
 
 class HostListView extends Component {
   render() {
     return (
-      <div style={{ height: 200, backgroundColor: "orange", margin: 10 }}>
+      <div style={{ height: 1000, backgroundColor: "orange", margin: 10 }}>
         Host List View
+        <HostFilter />
         {tempDevelopmentHosts.map(host => (
           <HostCard key={host} host={host} />
         ))}
