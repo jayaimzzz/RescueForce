37ee8e6f280617f2086faa17fc0cbc1565e3906b
Carl Smith <carlfsmithiii@gmail.com>
2018-12-20 13:02:07 -0500

    add ImageScroll and AnimalProfile to AnimalProfileView

---
+++ src/Components/AnimalProfileView/AnimalProfile.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AnimalProfile extends Component {
+  render() {
+    return (
+      <div style={{ height: 200, backgroundColor: "darkkhaki" }}>
+        Animal Profile
+      </div>
+    );
+  }
+}
+
+export { AnimalProfile };
--- src/Components/AnimalProfileView/index.js
+++ src/Components/AnimalProfileView/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import { ImageScroll } from "./ImageScroll";
+import { AnimalProfile } from "./AnimalProfile";
 
 class AnimalProfileView extends Component {
   render() {
     return (
-      <div style={{ height: 200, backgroundColor: "yellow", margin: 10 }}>
+      <div style={{ height: 350, backgroundColor: "yellow", margin: 10 }}>
         Animal Profile View
+        <ImageScroll />
+        <AnimalProfile />
       </div>
     );
   }
