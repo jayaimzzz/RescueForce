d0f2f7bff8602325d5eca68dda8ad2b6e9a0484e
Carl Smith <carlfsmithiii@gmail.com>
2018-12-20 10:31:31 -0500

    changed default text

--- src/Components/HostProfileView/index.js
+++ src/Components/HostProfileView/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class HostProfileView extends Component {
     render() {
         return (
-            <div style={{ height: 200, backgroundColor: "red", margin: 10 }}>Foster Family View</div>
+            <div style={{ height: 200, backgroundColor: "red", margin: 10 }}>Host Profile View</div>
         );
     };
 }
