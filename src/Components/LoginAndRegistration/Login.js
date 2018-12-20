d4e94d7339676bd88fb7a5ac2d6fc28badfcfca1
Clinton Johnson <dragoon1246224@gmail.com>
2018-12-20 14:37:34 -0500

    Login button from main page links to login page with login information.  Has basic functionality.

--- src/Components/LoginAndRegistration/Login.js
+++ src/Components/LoginAndRegistration/Login.js
@@ -1,3 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
+import { Form, Grid, Segment } from "semantic-ui-react";
 
-export const Login = () => <div>Log in</div>;
+class Login extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleCheckUserName = event => {
+    this.setState({
+      username: event.target.value
+    });
+  };
+
+  handleCheckPassword = event => {
+    this.setState({
+      password: event.target.value
+    });
+  };
+
+  handleLogin = event => {
+    this.props.login({
+      username: this.state.username,
+      password: this.state.password
+    });
+  };
+
+  render() {
+    return (
+      <Grid>
+        <Form method="get" className="ui large form">
+          <Segment>
+            <div className="field">
+              <input
+                // style={inputStyle}
+                type="text"
+                onChange={this.handleCheckUserName}
+                required
+                placeholder="Enter Username"
+              />
+            </div>
+            <div className="field">
+              <div className="ui left icon input">
+                <i className="lock icon" />
+                <input
+                  //   style={inputStyle}
+                  type="password"
+                  onChange={this.handleCheckPassword}
+                  required
+                  placeholder="Enter Password"
+                />
+              </div>
+              <button>
+                <div onClick={this.handleLogin}>Login</div>
+              </button>
+            </div>
+          </Segment>
+        </Form>
+      </Grid>
+    );
+  }
+}
+
+export default Login;
