8969f8521ff3072eb1dba83000f97f4d86fd0829
Carl Smith <carlfsmithiii@gmail.com>
2018-12-20 10:18:06 -0500

    Renamed FosterFamilyView to HostView

---
+++ src/Components/HostView/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HostView extends Component {
+    render() {
+        return (
+            <div style={{ height: 200, backgroundColor: "red", margin: 10 }}>Foster Family View</div>
+        );
+    };
+}
+
+export { HostView };
