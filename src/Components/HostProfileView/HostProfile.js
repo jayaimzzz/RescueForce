b6178cd58f0f535a8930707653e59afb88c5c49b
jayaimzzz <jameshorton@gmail.com>
2018-12-21 14:43:53 -0500

    added edit profile button.  it does nothing though

--- src/Components/HostProfileView/HostProfile.js
+++ src/Components/HostProfileView/HostProfile.js
@@ -1,17 +1,17 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Image, Container, Card } from "semantic-ui-react";
+import { Image, Button, Card, Icon } from "semantic-ui-react";
 
 class HostProfile extends Component {
   render() {
-    const styles = {
-      container: {
-        display: "inline-block",
-        backgroundColor: "BurlyWood",
-        height: 200,
-        width: "40vw"
-      }
-    };
+    // const styles = {
+    //   container: {
+    //     display: "inline-block",
+    //     backgroundColor: "BurlyWood",
+    //     height: 200,
+    //     width: "40vw"
+    //   }
+    // };
     const host = this.props.host;
     const hostProfilePic = host.photos[0];
     const shelter = this.props.shelter;
@@ -21,13 +21,18 @@ class HostProfile extends Component {
         <Image src={hostProfilePic} size="medium" />
         <Card.Content>
           <Card.Header>{host.name}</Card.Header>
-          
+          <Card.Description>Address: {host.address}</Card.Description>
+          <Card.Description>Phone Number: {host.phoneNumber}</Card.Description>
+          <Card.Description>Capacity: Cats:{host.capacity.cats}, Dogs:{host.capacity.dogs}</Card.Description>
           {host.approved && (
-            <Card.Description>
-              <Image src={shelterProfilePic} avatar="true" size="massive" />
+            <Card.Content>
+              <Image src={shelterProfilePic} avatar={true} size="massive" />
               Fostering for {shelter.name}
-            </Card.Description>
+            </Card.Content>
           )}
+          <Card.Content extra>
+          <Button onClick={()=>console.log('edit profile button clicked')}><Icon name='edit'/>Edit Profile</Button>
+          </Card.Content>
         </Card.Content>
       </Card>
     );
@@ -36,10 +41,12 @@ class HostProfile extends Component {
 
 const mapStateToProps = (state, props) => {
   const host = state.hosts.find(host => host.id === props.hostId);
+  const canEdit = state.loggedInUser.id === host.id
   const shelter = state.shelters.find(shelter => shelter.id === host.shelterId);
   return {
     host: host,
-    shelter: shelter
+    shelter: shelter,
+    canEdit: canEdit
   };
 };
 
