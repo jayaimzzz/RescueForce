d8bb59fa23b06e0e75699ae8e8cb04ac54dee1e1
Clinton Johnson <dragoon1246224@gmail.com>
2018-12-21 09:34:03 -0500

    Functionality input

---
+++ src/Components/AnimalProfileView/ImageUploader.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { connect } from "react-redux"
+
+class ImageUploader extends React.Component {
+   handleSubmit = event => {
+       event.preventDefault()
+       const formData = new FormData(event.target)
+       this.props.image(formData)
+   }
+  render() {
+      return (
+      <form onSubmit={this.handleSubmit}>
+          <input type="file" name="picture" id="picture"></input>
+          <button type="submit" value="Submit">Submit Upload</button>
+          {this.props.uploadImageResult}
+      </form>)
+  }
+}
+
+const mapStateToProps = state => {
+   return {
+     uploadImageResult: state.uploadImageResult
+   };
+ };
+
+const mapDispatchToProps = dispatch => {
+   return {
+     image: imageData => dispatch(uploadImage(imageData))
+   };
+ };
+
+export default connect(
+   mapStateToProps,
+   mapDispatchToProps
+ )(ImageUploader);
