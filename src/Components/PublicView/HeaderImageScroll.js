e6f27049209ad65e6f84203b8b59ce1cc430e765
Jennifer Browning <browningjen@yahoo.com>
2018-12-23 19:27:51 -0500

    created header image scroll in this branch

---
+++ src/Components/PublicView/HeaderImageScroll.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Image } from "semantic-ui-react";
+
+const styles = {
+  img: {
+    display: "inline-block",
+    borderRadius: "5px",
+    transition: "transform .2s"
+  },
+  segment: {
+    overflowX: "scroll",
+    whiteSpace: "nowrap",
+    borderRadius: "5px",
+    backgroundColor: "darkgrey"
+  }
+};
+
+class HeaderImageScroll extends Component {
+  render() {
+    const images = this.props.images;
+    return (
+      <div className="ui segment" style={styles.segment}>
+        {images.map((image, index) => (
+          <Image
+            style={styles.img}
+            size="small"
+            bordered
+            src={image}
+            key={`${this.props.animalId}ImgNum${index}`}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  return {
+    images: state.animals.find(animal => animal.id === props.animalId).pictures
+  };
+};
+
+const mapDispatchToProps = null;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HeaderImageScroll);
--- src/Components/PublicView/index.js
+++ src/Components/PublicView/index.js
@@ -1,13 +1,14 @@
 import React, { Component, Fragment } from "react";
-import { Header } from "./Header";
+import Header from "./Header";
+import HeaderImageScroll from "./HeaderImageScroll";
 
 class PublicView extends Component {
   render() {
     return (
       <Fragment>
-        <Header />
         <div style={{ height: 200, backgroundColor: "blueviolet" }}>
           Public View
+          <HeaderImageScroll animalId="1" />
         </div>
       </Fragment>
     );
