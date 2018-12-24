184280c6d64b4b462c71b862af2995faaf05bf3e
Jennifer Browning <browningjen@yahoo.com>
2018-12-24 17:00:47 -0500

    navigation bar set up and linked

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -4,7 +4,79 @@ class Nav extends Component {
   render() {
     return (
       <Fragment>
-        <div> Navigation BAr</div>
+        <div className="top" />
+        <div className="section-one" />
+        <div className="section-two" />
+        <div className="section-three" />
+        <div className="section-four" />
+        <div className="section-five" />
+        <nav
+          className="site-navigation page-navigation"
+          style={{
+            width: "100%",
+            backgroundColor: "#ebebeb",
+            position: "fixed",
+            top: "10%",
+            left: "0"
+          }}
+        >
+          <ul
+            className="ui header"
+            style={{ listStyleType: "none", display: "flex" }}
+          >
+            <li style={{ width: "100%" }}>
+              <a
+                href="#top"
+                style={{
+                  padding: "0.6em 10%",
+                  width: "80%"
+                }}
+              >
+                Top
+              </a>
+            </li>
+            <li style={{ width: "100%" }}>
+              <a
+                href="#section-one"
+                style={{ padding: "0.6em 10%", width: "80%" }}
+              >
+                One
+              </a>
+            </li>
+            <li style={{ width: "100%" }}>
+              <a
+                href="#section-two"
+                style={{ padding: "0.6em 10%", width: "80%" }}
+              >
+                Two
+              </a>
+            </li>
+            <li style={{ width: "100%" }}>
+              <a
+                href="#section-three"
+                style={{ padding: "0.6em 10%", width: "80%" }}
+              >
+                Three
+              </a>
+            </li>
+            <li style={{ width: "100%" }}>
+              <a
+                href="#section-four"
+                style={{ padding: "0.6em 10%", width: "80%" }}
+              >
+                Four
+              </a>
+            </li>
+            <li style={{ width: "100%" }}>
+              <a
+                href="#section-five"
+                style={{ padding: "0.6em 10%", width: "80%" }}
+              >
+                Five
+              </a>
+            </li>
+          </ul>
+        </nav>
       </Fragment>
     );
   }
