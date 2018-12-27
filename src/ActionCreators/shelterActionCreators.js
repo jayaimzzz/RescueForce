3042158c38d221c998d06c3dcedefd51ef8e22a2
jayaimzzz <jameshorton@gmail.com>
2018-12-27 16:12:42 -0500

    not much is working

---
+++ src/ActionCreators/shelterActionCreators.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { API_DOMAIN  } from "../Constants";
+
+export const GET_SHELTER_BY_ID = "GET_SHELTER_BY_ID"
+
+export const getShelterById = shelterId => {
+    return function(dispatch) {
+        axios
+            .get(API_DOMAIN + "/shelters/" + shelterId)
+            .then(res =>  {
+                if (res.status === 200) {
+                    console.log(res.data.data)
+                    dispatch({
+                        type: GET_SHELTER_BY_ID,
+                        payload: res.data.data
+                    })
+                } else {
+                    console.log(res);
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+}
--- src/Reducers/SheltersReducer.js
+++ src/Reducers/SheltersReducer.js
@@ -1,9 +1,14 @@
 import { shelters } from '../dummyData';
+import { GET_SHELTER_BY_ID } from '../ActionCreators'
 
-const initState = shelters;
+const initState = [];
 
 export const SheltersReducer = (state = initState, action) => {
+    console.log(action.payload)
     switch(action.type){
+        case GET_SHELTER_BY_ID:
+        state = state.slice()
+        return state.push(action.payload)
         default: return state
     }
 }
