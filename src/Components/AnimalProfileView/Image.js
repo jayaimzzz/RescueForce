371b55ec526f929b50cbffdd2ba0a99faa3fab27
Carl Smith <carlfsmithiii@gmail.com>
2019-01-07 17:54:00 -0500

    image order can be changed at modal component level

--- src/Components/AnimalProfileView/Image.js
+++ src/Components/AnimalProfileView/Image.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import { Image } from 'semantic-ui-react';
+import React from "react";
+import { Image } from "semantic-ui-react";
 
-export const GalleryImage = ({ photo }) => {
-    return (
-        <Image src={photo.src} />
-    );
+const imgWithClick = { cursor: "pointer" };
+
+export const GalleryImage = ({
+  index,
+  onClick,
+  photo,
+  margin,
+  direction,
+  top,
+  left
+}) => {
+  const imgStyle = { margin };
+  if (direction === "column") {
+    imgStyle.position = "absolute";
+    imgStyle.left = left;
+    imgStyle.top = top;
+  }
+
+  const handleClick = event => {
+      onClick(event, { photo, index });
+  };
+
+  return (
+    <img
+      style={onClick ? { ...imgStyle, ...imgWithClick } : imgStyle}
+      {...photo}
+      onClick={onClick ? handleClick : null}
+      alt="img"
+    />
+  );
 };
--- src/Components/AnimalProfileView/ImageEdit.js
+++ src/Components/AnimalProfileView/ImageEdit.js
@@ -10,7 +10,7 @@ import { GalleryImage } from './Image';
 
 const SortableImage = SortableElement(GalleryImage);
 const SortableGallery = SortableContainer(({photos}) => {
-  return <Gallery photos={photos} columns={5} ImageComponent={SortableImage} />;
+  return <Gallery photos={photos} columns={4} ImageComponent={SortableImage} />;
 });
 
 class ImageEdit extends Component {
@@ -18,11 +18,17 @@ class ImageEdit extends Component {
     images: this.props.images.map(image => ({src: image, width: 4, height: 3})) || []
   };
 
+  onSortEnd = ({oldIndex, newIndex}) => {
+    this.setState({
+      images: arrayMove(this.state.images, oldIndex, newIndex)
+    });
+  };
+
   render() {
     return (
-      <Modal trigger={<Button>Edit Images</Button>}>
+      <Modal style={{width: '80vw'}} trigger={<Button>Edit Images</Button>}>
         <Modal.Header>Edit Images</Modal.Header>
-        <SortableGallery axis={"xy"} photos={this.state.images} />
+        <SortableGallery axis={"xy"} photos={this.state.images} onSortEnd={this.onSortEnd}/>
       </Modal>
     );
   }
