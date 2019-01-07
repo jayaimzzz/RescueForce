5d8ad7a812a7e5c573faee0e5a8c3e6f286b8824
jayaimzzz <jameshorton@gmail.com>
2019-01-07 11:48:35 -0500

    fixed error of showing all animals at first render

--- src/Components/AnimalList/index.js
+++ src/Components/AnimalList/index.js
@@ -1,16 +1,14 @@
 import React, { Component, Fragment } from "react";
 import { AnimalCard } from "./AnimalCard";
-import { connectRouter } from "connected-react-router";
 import { connect } from "react-redux";
 import { getAnimals } from "../../ActionCreators";
 
 class AnimalList extends Component {
-  // componentDidMount() {
-  //   this.props.getAnimals(this.props.filter);
-  // }
-
-  render() {
+  componentDidMount() {
     this.props.getAnimals(this.props.filter);
+  }
+  
+  render() {
     return (
       <Fragment>
         {this.props.animals.map(animal => (
@@ -21,24 +19,16 @@ class AnimalList extends Component {
   }
 }
 
-// const mapStateToProps = (state, props) => {
-//   if (props.hostId){
-//     return {
-//       animals: state.animals.filter(animal => animal.hostId === props.hostId)
-//     };
-//   } else {
-//     return {
-//       animals: state.animals
-//     }
-//   }
-// };
+const mapStateToProps = (state, props) => {
+  const filter = props.filter;
+  const filterKey = Object.keys(filter)[0];
+  return {
+    animals: state.animals.filter(animal => animal[filterKey] === filter[filterKey])
+  }
+};
 
 const mapDispatchToProps = dispatch => ({
   getAnimals: filter => dispatch(getAnimals(filter))
-});
-
-const mapStateToProps = (state, props) => ({
-  animals: state.animals
 });
 
 export default connect(
