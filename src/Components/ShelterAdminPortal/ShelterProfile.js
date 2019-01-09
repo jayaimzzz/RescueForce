6324c0d9fd53743f66d803e6d36de137df889a24
Carl Smith <carlfsmithiii@gmail.com>
2019-01-09 18:37:57 -0500

    Created shell of shelter update modal

--- src/Components/ShelterAdminPortal/ShelterProfile.js
+++ src/Components/ShelterAdminPortal/ShelterProfile.js
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Image, Button, Card, Icon } from "semantic-ui-react";
+import ModalUpdate from "./ModalUpdate";
 
 class ShelterProfile extends Component {
   render() {
     const shelter = this.props.shelter;
     return (
-      <Card style={{margin: 'auto', width: '40vw'}}>
+      <Card style={{ margin: "auto", width: "40vw" }}>
         <Image src={shelter.photos[0]} />
         <Card.Content>
           <Card.Header>{shelter.name}</Card.Header>
           <Card.Description>Address: {shelter.address}</Card.Description>
-          <Card.Description>Phone Number: {shelter.phoneNumber}</Card.Description>
+          <Card.Description>
+            Phone Number: {shelter.phoneNumber}
+          </Card.Description>
           <Card.Description>Email: {shelter.email}</Card.Description>
-          <Card.Description>Capacity: Cats:{shelter.capacity && shelter.capacity.cats}, Dogs:{shelter.capacity && shelter.capacity.dogs}</Card.Description>
+          <Card.Description>
+            Capacity: Cats:{shelter.capacity && shelter.capacity.cats}, Dogs:
+            {shelter.capacity && shelter.capacity.dogs}
+          </Card.Description>
 
           {this.props.canEdit && (
             <Card.Content extra>
-            <Button onClick={()=>console.log('edit profile button clicked')}><Icon name='edit'/>Edit Profile</Button>
+              <ModalUpdate />
             </Card.Content>
-            )}
+          )}
         </Card.Content>
       </Card>
     );
