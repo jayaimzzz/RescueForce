00da81918a5c4a3c94619c72a22a03bff817a07f
Clinton Johnson <dragoon1246224@gmail.com>
2019-01-11 10:11:26 -0500

    Fixed date for animal profile.

--- src/Components/AnimalProfileView/AnimalProfile.js
+++ src/Components/AnimalProfileView/AnimalProfile.js
@@ -16,11 +16,14 @@ class AnimalProfile extends Component {
   
 
   render() {
+    
     const animal = this.props.animal;
+    const host = this.props.host
     const data = {
       "id":this.props.animal._id,
       "hostId":this.props.loggedInUserId,
-      "status":"foster-only"
+      "status":"foster-only",
+      "host":this.props.host,
     }
 
     return (
@@ -30,10 +33,12 @@ class AnimalProfile extends Component {
            <Card.Header>{animal.name}</Card.Header>
                <Card.Description>Species: {animal.species}</Card.Description>
                <Card.Description>Breed: {animal.breed}</Card.Description>
-               <Card.Description>DOB: {animal.dob}</Card.Description>
+               <Card.Description>DOB: {moment(animal.dob).format('MM/DD/YYYY')}</Card.Description>
                <Card.Description>Sex: {animal.sex}</Card.Description>
                <Card.Description>About: {animal.about}</Card.Description>
-               <Card.Description>Host Id: {animal.hostId ? animal.hostId._id : ''}</Card.Description>
+               <Card.Description>
+                 Host: {host.name}
+               </Card.Description>
                <Card.Description>Status: {animal.status}</Card.Description>
                <Card.Description>{animal.name} {animal.animalFriendly ? "is" : "is not"} animal-friendly. </Card.Description>
                <Card.Description>{animal.name} {animal.peopleFriendly ? "is" : "is not"} people-friendly.  </Card.Description>
