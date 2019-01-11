bfed36bee682cdded89f4dfbce0e8ee96e649c1c
Carl Smith <carlfsmithiii@gmail.com>
2019-01-11 13:16:05 -0500

    filter animals by breed

--- src/Components/AnimalListView/AnimalFilter.js
+++ src/Components/AnimalListView/AnimalFilter.js
@@ -1,8 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
+import {
+  Form,
+  Button,
+  Modal,
+  Icon,
+  Header,
+  Segment,
+  Radio
+} from "semantic-ui-react";
 
-
-export const AnimalFilter = () => (
-  <div style={{ height: 80, backgroundColor: "green", margin: 10 }}>
-    Filter
-  </div>
+export const AnimalFilter = props => (
+  <Segment
+    style={{
+      width: "100%",
+      backgroundColor: "lightgrey",
+      margin: "auto",
+      marginTop: 40
+    }}
+  >
+    <Form>
+      <Form.Select name='breed' options={props.breeds} onChange={props.filters.handleChange}/>
+    </Form>
+  </Segment>
 );
--- src/Components/AnimalListView/index.js
+++ src/Components/AnimalListView/index.js
@@ -1,18 +1,93 @@
-import React, { Component, Fragment } from "react";
-
+import React, { Component } from "react";
+import { Segment, Grid } from "semantic-ui-react";
+import { connect } from "react-redux";
 import AnimalList from "../AnimalList";
 import { AnimalFilter } from "./AnimalFilter";
 
 class AnimalListView extends Component {
+  state = {
+    filter: {}
+  };
+
+  handleFilterNullableRadioChange = (event, { name, value }) => {
+    const filter = { ...this.state.filter };
+    if (this.state.filter[name] === value) {
+      delete filter[name];
+    } else {
+      filter[name] = value;
+    }
+    this.setState({ filter });
+  };
+
+  handleFilterChange = (event, { name, value }) => {
+    const filter = {...this.state.filter };
+    if (value === 'Show All') {
+      delete filter[name];
+    } else {
+      filter[name] = value;
+    }
+    this.setState({
+      filter
+    });
+  };
+
   render() {
-    const { animalType } = this.props;
+    const animals = this.props.animals.filter(animal => {
+      for (let key in this.state.filter) {
+        if (animal[key] !== this.state.filter[key]) {
+          return false;
+        }
+      }
+      return true;
+    });
     return (
-      <Fragment>
-        <AnimalFilter />
-        <AnimalList filter={{ species: animalType }} />
-      </Fragment>
+      <Segment style={{ maxWidth: 1500, margin: "auto" }}>
+        <Grid>
+          <Grid.Column width={10}>
+            <AnimalList animals={animals} />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <AnimalFilter
+              breeds={this.props.breeds}
+              filters={{
+                handleChange: this.handleFilterChange,
+                handleNullableRadioChange: this.handleFilterNullableRadioChange
+              }}
+            />
+          </Grid.Column>
+        </Grid>
+      </Segment>
     );
   }
 }
 
-export default AnimalListView;
+const mapStateToProps = (state, props) => {
+  const animals = state.animals.filter(
+    animal => animal.species === props.animalType
+  );
+  return {
+    animals,
+    breeds: animals
+      .reduce(
+        (breedlist, animal) => {
+          if (animal.breed && !breedlist.includes(animal.breed.toLowerCase())) {
+            breedlist.push(animal.breed.toLowerCase());
+            return breedlist;
+          } else {
+            return breedlist;
+          }
+        },
+        ['Show All']
+      ).sort((a, b) => {
+        if (b === 'Show All' || a.toLowerCase() < b.toLowerCase()) return -1;
+      })
+      .map(breed => ({ text: breed, value: breed }))
+  };
+};
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AnimalListView);
