271526f3864aa98fa6b35201b3e0a5d318e788e7
jayaimzzz <jameshorton@gmail.com>
2019-01-17 13:02:41 -0500

    Updates

---
+++ precache-manifest.65bad44912c839e3c4a916f052f1782f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "13d29e575f0d6e29754b",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/css/main.3f1fe3c9.chunk.css"
+  },
+  {
+    "revision": "13d29e575f0d6e29754b",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/js/main.13d29e57.chunk.js"
+  },
+  {
+    "revision": "91dea6ddf54e4d48c53f",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/css/1.322b4531.chunk.css"
+  },
+  {
+    "revision": "91dea6ddf54e4d48c53f",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/js/1.91dea6dd.chunk.js"
+  },
+  {
+    "revision": "d2228b94bd7c899f1711",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/js/runtime~main.d2228b94.js"
+  },
+  {
+    "revision": "9b6a82c0510a9ca41c994a0a96a42005",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/picComingSoon.9b6a82c0.png"
+  },
+  {
+    "revision": "8446cf0c8eceabbd8140984f9275cc93",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/dog-cat01.8446cf0c.png"
+  },
+  {
+    "revision": "65b9b170f76ba0267daf1b533f4f51ea",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/LeftSideGraphic.65b9b170.png"
+  },
+  {
+    "revision": "6a2e679f9be1759325a2f63bcb6731bd",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/RightSideGraphic.6a2e679f.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "http//jayaimzzz.github.io/RescueForce/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "db25e2aa2b98eef441e76efcfc88842f",
+    "url": "http//jayaimzzz.github.io/RescueForce/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "http//jayaimzzz.github.io/RescueForce/precache-manifest.65bad44912c839e3c4a916f052f1782f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("http//jayaimzzz.github.io/RescueForce/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
